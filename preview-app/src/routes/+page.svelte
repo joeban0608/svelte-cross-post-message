3f392d3a8d67dfed0b960b13f20dabd1adda1e38
hongbangzhou <joeanddaneil5@gmail.com>
2025-01-09 02:13:54 +0800

    feat: preview-app add daisyui

--- preview-app/src/routes/+page.svelte
+++ preview-app/src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-4xl font-bold">Welcome to preview page!</h1>
+<p>Here is try to open in iframe</p>
+<a href="http://localhost:5173" class="btn btn-link">click me to redirect main-app</a>
+<button class="btn btn-primary" onclick={()=> alert('here is preview page')}>Click to alert</button>
