d746b82b3db6fe41e00c6c99bf348c48abc57f44
hongbangzhou <joeanddaneil5@gmail.com>
2025-01-09 03:10:41 +0800

    fix: prettier

--- preview-app/src/routes/+page.svelte
+++ preview-app/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <main class="flex flex-col gap-4">
-  <h1 class="text-4xl font-bold">Welcome to preview page!</h1>
-  <p>Here is try to open in iframe</p>
-  <a href="http://localhost:5173" class="btn btn-link">click me to redirect main-app</a>
-  <button class="btn btn-primary" onclick={()=> alert('here is preview page')}>Click to alert</button>
+	<h1 class="text-4xl font-bold">Welcome to preview page!</h1>
+	<p>Here is try to open in iframe</p>
+	<a href="http://localhost:5173" class="btn btn-link">click me to redirect main-app</a>
+	<button class="btn btn-primary" onclick={() => alert('here is preview page')}>
+		Click to alert
+	</button>
 </main>
