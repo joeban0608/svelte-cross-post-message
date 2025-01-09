4e09c7a868ce4ffedb421f0b9f41cd0acc1b9079
joeban <joeanddaneil5@gmail.com>
2025-01-09 17:35:13 +0800

    feat: add mouse inspector

--- preview-html/script.js
+++ preview-html/script.js
@@ -1,8 +1,13 @@
+const originalBorders = new Map();
+
 function iframeClickListener(event) {
   const mouseEvent = event;
   event.stopPropagation(); // prevent js
   event.preventDefault(); // prevent a link
+
   const target = mouseEvent.target;
+  iframeMouseOutTracker(event)
+  
   window.parent.postMessage(
     {
       type: "select-element-end",
@@ -11,15 +16,38 @@ function iframeClickListener(event) {
     "*"
   );
 }
-window.addEventListener("message", (event) => {
-  if (event.data.type === "select-element-start") {
-    window.document.body.addEventListener("click", iframeClickListener, true);
+
+function iframeMouseOverTracker(e) {
+  if (e.target && e.target.tagName !== "HTML" && e.target.tagName !== "BODY") {
+    // Store the original border style if not already stored
+    if (!originalBorders.has(e.target)) {
+      originalBorders.set(e.target, e.target.style.border);
+    }
+    e.target.style.border = "2px solid red";
+    e.target.classList.add("breathing-border");
   }
+}
+
+function iframeMouseOutTracker(e) {
+  if (e.target && e.target.tagName !== "HTML" && e.target.tagName !== "BODY") {
+    // Restore the original border style
+    e.target.style.border = originalBorders.get(e.target) || "";
+    e.target.classList.remove("breathing-border");
+  }
+}
+
+window.addEventListener("message", (event) => {
+  const bodyEl = window.document.body;
+
+  if (event.data.type === "select-element-start") {
+    bodyEl.addEventListener("click", iframeClickListener, true);
+    bodyEl.addEventListener("mouseover", iframeMouseOverTracker);
+    bodyEl.addEventListener("mouseout", iframeMouseOutTracker);
+  }
+
   if (event.data.type === "select-element-finish") {
-    window.document.body.removeEventListener(
-      "click",
-      iframeClickListener,
-      true
-    );
+    bodyEl.removeEventListener("click", iframeClickListener, true);
+    bodyEl.removeEventListener("mouseout", iframeMouseOutTracker);
+    bodyEl.removeEventListener("mouseover", iframeMouseOverTracker);
   }
 });
